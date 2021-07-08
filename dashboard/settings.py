acf5ef80f1e559aeed997304c6df6c25cc3c580c
Marcia Marques <marcia.marques@alumni.usp.br>
2021-07-08 20:36:58 -0300

    Create Campaign model

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -130,3 +130,7 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
--- data/models.py
+++ data/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Campaign(models.Model):
+    name = models.CharField(max_length=25)
+    description = models.TextField(max_length=300)
+    latitude = models.DecimalField(max_digits=13, decimal_places=9)
+    longitude = models.DecimalField(max_digits=13, decimal_places=9)
+    picture = models.ImageField(upload_to='pictures/')
+    file = models.FileField(upload_to='files/')
+
+    def __str__(self):
+        return self.name
