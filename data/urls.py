349095ac7739b73c8ced350304bdf13d26ede663
Marcia Marques <marcia.marques@alumni.usp.br>
2021-07-08 23:00:11 -0300

    Add CampaignListView

---
+++ data/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .views import CampaignListView
+
+urlpatterns = [
+    path('campaigns/', CampaignListView.as_view(), name='campaign_list'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- data/views.py
+++ data/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Campaign
+
+
+class CampaignListView(ListView):
+    model = Campaign
+    template_name = 'data/campaign_list.html'
+    context_object_name = 'campaigns'
