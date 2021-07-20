66ad2b98ff7bfca22cfe1cdfc7b7ddbbb74cc526
Marcia Marques <marcia.marques@alumni.usp.br>
2021-07-20 00:28:11 -0300

    New form created with function

--- data/models.py
+++ data/models.py
@@ -14,6 +14,7 @@ class Campaign(models.Model):
     end_date = models.DateField(max_length=20, blank=True, null=True)
     var1 = models.CharField(max_length=10, blank=True, null=True)
     var2 = models.CharField(max_length=10, blank=True, null=True)
+    mylist = models.TextField(max_length=150, blank=True, null=True)
 
     def save(self, *args, **kwargs):
         df = pd.read_csv(self.file)
@@ -22,6 +23,13 @@ class Campaign(models.Model):
         self.end_date = df.DATE_TIME.max().strftime('%Y-%m-%d')
         self.var1 = [x for x in df.columns if 'CO2_dry' in x][0]
         self.var2 = [x for x in df.columns if 'CH4_dry' in x][0]
+
+        mylist = str(list(df.columns))
+        mapping = [('[', ''), (']', ''), ("'", ''), (' ', '')]
+        for k, v in mapping:
+            mylist = mylist.replace(k, v)
+        self.mylist = mylist
+
         super().save(*args, **kwargs)
 
     def __str__(self):
--- graphs/forms.py
+++ graphs/forms.py
@@ -1,42 +1,61 @@
 from django import forms
 
-from data.models import Campaign
+# from data.models import Campaign
 
 
 class DateInput(forms.DateInput):
     input_type = 'date'
 
 
-class DateRangeModelForm(forms.ModelForm):
-    class Meta:
-        model = Campaign
-        fields = ['start_date', 'end_date', 'var1', 'var2']
-        widgets = {
-            'start_date': DateInput(attrs={'class': 'form-control'}),
-            'end_date': DateInput(attrs={'class': 'form-control'}),
-            'var1': forms.TextInput(attrs={'class': 'form-control'}),
-            'var2': forms.TextInput(attrs={'class': 'form-control'})}
+# class DateRangeModelForm(forms.ModelForm):
+#     class Meta:
+#         model = Campaign
+#         fields = ['start_date', 'end_date', 'var1', 'var2']
+#         widgets = {
+#             'start_date': DateInput(attrs={'class': 'form-control'}),
+#             'end_date': DateInput(attrs={'class': 'form-control'}),
+#             'var1': forms.TextInput(attrs={'class': 'form-control'}),
+#             'var2': forms.TextInput(attrs={'class': 'form-control'})}
 
 
-class DateRangeForm(forms.Form):
-    start_date = forms.DateField(widget=forms.DateInput(
-        attrs={'class': 'form-control form-control-sm'}),
-    )
-    end_date = forms.DateField(widget=forms.DateInput(
-        attrs={'class': 'form-control form-control-sm'}),
-    )
+def DateRangeFormFunction(var_choices):
 
-    def clean(self):
-        cleaned_data = super().clean()
-        start_date = cleaned_data.get("start_date")
-        end_date = cleaned_data.get("end_date")
-        if start_date and end_date:
-            if start_date >= end_date:
-                raise forms.ValidationError(
-                    "Initial date must be less than end date")
-            # if start_date < min_date:
-            #     raise forms.ValidationError(
-            #         "Data starts at " + str(min_date)
-            # if end_date > max_date:
-            #     raise forms.ValidationError(
-            #         "Data starts at " + str(max_date)
+    class DateRangeForm(forms.Form):
+        start_date = forms.DateField(widget=DateInput(
+            attrs={'class': 'form-control form-control-sm'}),
+        )
+        end_date = forms.DateField(widget=DateInput(
+            attrs={'class': 'form-control form-control-sm'}),
+        )
+        var1 = forms.ChoiceField(widget=forms.Select(
+            attrs={'class': 'form-control form-control-sm'}),
+            choices=var_choices)
+        var2 = forms.ChoiceField(widget=forms.Select(
+            attrs={'class': 'form-control form-control-sm'}),
+            choices=var_choices)
+
+    return DateRangeForm
+
+
+# class DateRangeForm(forms.Form):
+#     start_date = forms.DateField(widget=forms.DateInput(
+#         attrs={'class': 'form-control form-control-sm'}),
+#     )
+#     end_date = forms.DateField(widget=forms.DateInput(
+#         attrs={'class': 'form-control form-control-sm'}),
+#     )
+
+#     def clean(self):
+#         cleaned_data = super().clean()
+#         start_date = cleaned_data.get("start_date")
+#         end_date = cleaned_data.get("end_date")
+#         if start_date and end_date:
+#             if start_date >= end_date:
+#                 raise forms.ValidationError(
+#                     "Initial date must be less than end date")
+#             # if start_date < min_date:
+#             #     raise forms.ValidationError(
+#             #         "Data starts at " + str(min_date)
+#             # if end_date > max_date:
+#             #     raise forms.ValidationError(
+#             #         "Data starts at " + str(max_date)
